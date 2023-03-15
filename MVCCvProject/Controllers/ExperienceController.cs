6cfab12871e56acccd9d2399b657d783d2f569ae
Berke <85216724+blorein-code@users.noreply.github.com>
2023-03-16 00:39:11 +0300

    Experience controller oluşturuldu ve ona ilgili index.html bağlandı.

---
+++ MVCCvProject/Controllers/ExperienceController.cs
@@ -0,0 +1,20 @@
+﻿using MVCCvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCvProject.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        // GET: Experience
+        ExperienceRepository repo = new ExperienceRepository();
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+    }
+}
--- MVCCvProject/Repositories/HobbyRepository.cs
+++ MVCCvProject/Repositories/HobbyRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using MVCCvProject.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MVCCvProject.Repositories
 {
-    public class HobbyRepository
+    public class HobbyRepository : GenericRepository<HobbyTable>
     {
     }
 }
